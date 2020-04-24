47d3bffeeda8ae4a96a888467a37de5c25061f17
skanjarla <skanjarla@zyclyx.com>
2020-04-24 11:32:36 +0530

    Add html templates for email

--- index.js
+++ index.js
@@ -1,118 +1,135 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const multer = require('multer');
-const fs = require('fs');
-const dotenv = require('dotenv');
+const express = require("express");
+const nodemailer = require("nodemailer"); 
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser'); 
+const dotenv = require("dotenv");
+const fs = require('fs'); 
+const date = require('date-and-time'); 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const user = process.env.USER;
 const password = process.env.PASSWORD;
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/')
-    },
-    filename: function (req, file, cb) {
-        var file = file.originalname
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-        cb(null, file)
-    }
-})
 
-const upload = multer({ storage: storage })
-
+ 
 
 // send applicaton data to email
-app.post('/application-form', upload.single('file'), function (req, res) {
-    const name = req.body.name;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    const subject = req.body.subject;
-    const resume = req.file.originalname;
-    const message = req.body.message;
-    console.log(name, email, phone, resume, subject, message);
+app.post("/application-form", function(req, res) {
+  console.log(req.files)
+  const name = `${req.body.firstname} ${req.body.lastname}`;
+  const email = req.body.email;
+  const phone = `(+${req.body.countryCode}) ${req.body.phone}`;  
+  const resume = req.files.resume;
+  const message = req.body.message;
+  const website = req.body.website;
+  const role = req.body.role;
+  console.log(req.files);
+  resume.mv('./uploads/' + req.files.resume);
 
-    const applicationFormTransport = nodemailer.createTransport({
-        service: 'gmail',
-        secure: 'false',
-        port: '25',
-        auth: {
-            user: user,
-            pass: password
-        },
-        tls: {
-            rejectUnauthorized: false
-        },
-
-    });
-
-    const mailOption = {
-        // from:'srikanthkanjarla@gmail.com',
-        to: 'sandeep.reddy@zyclyx.com',
-        subject: 'New Job Application',
-        text: 'Name : ' + name + '\n' + 'Email : ' + email + '\n' + 'Phone : ' + phone + '\n' + 'Subject : ' + subject + '\n' + 'Resume : ' + resume + '\n' + 'Message : ' + message + '\n',
-        attachments: [{ filename: 'image.png', content: fs.createReadStream(`./uploads/${resume}`) }]
+  const applicationFormTransport = nodemailer.createTransport({
+    service: "gmail",
+    secure: "false",
+    port: "25",
+    auth: {
+      user: user,
+      pass: password
+    },
+    tls: {
+      rejectUnauthorized: false
     }
+  });
 
-    applicationFormTransport.sendMail(mailOption, function (req, res) {
-        if (req) {
-            console.log("Message Not Sent :(")
-            console.log(req)
-        }
-        else {
-            console.log("Message Sent Succesfully!")
-        }
-    })
-    res.send('Hello World 2!')
+  const mailOption = {     
+    to: "skanjarla@zyclyx.com",
+    subject: `New Job Application for ${role} from virtech website ${date.format(new Date(), 'DD/MM/YYYY HH:mm A')}`,     
+    html:`<p>Dear HR Team, </p>
+    <p>Please find the candidate profile and attached resume for ${role} role below</p>
+    <table style="border-collapse:collapse">
+    <tr style="background-color:#00bdd5;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #0D93B8;">Source</td><td style="font-weight:600;padding:1rem;">${website} Website</td></tr>          
+    <tr style="background-color:#0d93b833;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #00bdd5;">Fullname</td><td style="font-weight:600;padding:1rem;">${name}</td></tr>
+    <tr style="background-color:#00bdd5;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #0D93B8;">Email</td><td style="font-weight:600;padding:1rem;">${email}</td></tr>
+    <tr style="background-color:#0d93b833;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #00bdd5;">Phone</td><td style="font-weight:600;padding:1rem;">${phone}</td></tr>
+    <tr style="background-color:#00bdd5;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #0D93B8;">Position</td><td style="font-weight:600;padding:1rem;">${role}</td></tr>
+    <tr style="background-color:#0d93b833;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #00bdd5;">Message</td><td style="font-size:0.9rem;padding:1rem;max-width:400px;">${message}</td></tr>
+    </table>
+    <p>Thank you</p>`,     
+    attachments: [
+      {
+        filename: resume.name,
+        content: fs.createReadStream(`./uploads/${resume.name}`)  
+      }
+    ]
+  };
 
-})
+  applicationFormTransport.sendMail(mailOption, (error,info)=> {
+    if (error) {       
+      console.log("Error"+error);
+    } else {
+      console.log("Success" + info);
+      res.send(info);
+    }
+  });  
+});
 
 // send contact form data to email
-app.post('/contact', upload.single(''), function (req, res) {
-    const name = req.body.name;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    const subject = req.body.subject;
-    const message = req.body.message;
-    console.log(name, email, phone, subject, message);
-
-    const applicationFormTransport = nodemailer.createTransport({
-        service: 'gmail',
-        secure: 'false',
-        port: '25',
-        auth: {
-            user: user,
-            pass: password
-        },
-        tls: {
-            rejectUnauthorized: false
-        },
-
-    });
-
-    const mailOption = {         
-        from: 'sandeep.reddy@zyclyx.com',
-        to: 'sandeep.reddy@zyclyx.com',
-        subject: 'New Job Application',
-        // text: 'Name : ' + name + '\n' + 'Email : ' + email + '\n' + 'Phone : ' + phone + '\n' + 'Subject : ' + subject + '\n' + 'Message : ' + message + '\n',
-        text:`Name : ${name} \n Email : ${email} \n Phone : ${phone} \n Subject : ${subject} \n Message : ${message}`,
-        // attachments: [{ filename: 'image.png', content: fs.createReadStream(`./uploads/${resume}`) }]
+app.post("/contact", function(req, res) {
+                
+  const fullname = req.body.fullname;
+  const email = req.body.email;
+  const phone = `(+${req.body.countryCode}) ${req.body.phone}`;;   
+  const message = req.body.message;
+  const website = req.body.website;   
+   
+ console.log(req.body)
+  const applicationFormTransport = nodemailer.createTransport({
+    service: "gmail",
+    secure: "false",
+    port: "25",
+    auth: {
+      user: user,
+      pass: password
+    },
+    tls: {
+      rejectUnauthorized: false
     }
+  });
 
-    applicationFormTransport.sendMail(mailOption, function (req, res) {
-        if (req) {
-            console.log("Message Not Sent :(")
-            console.log(req)
-        }
-        else {
-            console.log("Message Sent Succesfully!")
-            //res.send('Contact Msessage Sent Successfully')
-        }
-    })
-    
-})
+  const mailOption = {  
+    to: "skanjarla@zyclyx.com",
+    subject: "New Job Application",
+    subject: `New Enquiry from ${website} website ${date.format(new Date(), 'DD/MM/YYYY HH:mm A')}`,     
+    html:`<p>Dear Team, </p>
+    <p>Please find the enquiry details from ${website} website contact us form below</p>
+    <table style="border-collapse:collapse">
+    <tr style="background-color:#00bdd5;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #0D93B8;">Source</td><td style="font-weight:600;padding:1rem;">${website} Website</td></tr>          
+    <tr style="background-color:#0d93b833;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #00bdd5;">Fullname</td><td style="font-weight:600;padding:1rem;">${fullname}</td></tr>
+    <tr style="background-color:#00bdd5;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #0D93B8;">Email</td><td style="font-weight:600;padding:1rem;">${email}</td></tr>
+    <tr style="background-color:#0d93b833;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #00bdd5;">Phone</td><td style="font-weight:600;padding:1rem;">${phone}</td></tr>     
+    <tr style="background-color:#00bdd5;font-size:1.2rem;"><td style="padding:1rem;border-right:1px solid #0D93B8;">Message</td><td style="font-size:0.9rem;padding:1rem;max-width:400px;">${message}</td></tr>
+    </table>
+    <p>Thank you</p>`,
+  };
 
+  applicationFormTransport.sendMail(mailOption, function(error,info) {
+    if (error) {       
+      console.log("Error"+error);
+    } else {
+      console.log("Success" + info);
+      res.send(info);
+    }
+  });
+});
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+app.listen(port, () => console.log(`app listening on port ${port}!`));
